cae6ea861e335213f6360ac7aa3b2288c1df70d1
macosta2015 <macosta2015@my.fit.edu>
2022-06-26 21:27:04 -0400

    Update server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const routes = require('./routes');
+
 //import sequelize connection
 
 const app = express();
-const PORT = process.env.PORT || 3006;
+const PORT = process.env.PORT || 3306;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +14,6 @@ app.use(routes);
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
-  
+
 });
 
